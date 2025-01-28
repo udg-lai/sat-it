6853d10ecf4b4b02d9bc552e5ef855e23909a916
wilberquito <wbq.software@gmail.com>
2025-01-28 15:31:52 +0100

    feat: sketching out CNF to view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,81 @@
 <script lang="ts">
+	import Literal from '$lib/literal.svelte.ts';
   import LiteralComponent from '$lib/LiteralComponent.svelte';
-  import {Variable} from '$lib/variable.ts';
+  import Variable, {IdVariableMap} from '$lib/variable.svelte.ts';
+  import { Toggle } from 'flowbite-svelte';
 
-  interface Literal {
-    value: number;
-  }
+  interface IdVariable { id: number, variable: Variable }
+  type RaWCNF = number[][]
+  type CNF = Literal[][]
 
-  const variables = new Set([1, 2, 3])
-  const variablesRef = Array.from(variables).reduce((D, n) => {
-    D[n] = rawToVariableInstance(n);
+  const rawCNF: RaWCNF = [
+    [1, -2, 3],
+    [3, 1, 2]
+  ]
+
+  const variables = new Set(
+    rawCNF.flatMap((clause) => clause.map(literal => Math.abs(literal)))
+  )
+
+  const variablesMap = Array.from(variables).reduce((D, n) => {
+    D.set(n, rawVariableToVariable(n))
     return D;
-  }, {} as Record<number, Variable>);
+  }, new IdVariableMap());
 
+  const variablesList: IdVariable[] = Array.from(
+    variablesMap.entries().map(([a, b]) => {
+      return {
+        'id': a,
+        'variable': b
+      }
+    })
+  )
 
-  const clause = [1, -2, 3].map(l => {
-    return {value: l}
-  })
+  const cnf: CNF = rawCNF.map((clause) => clause.map(newLiteral)) 
+  const clause = cnf[0]
 
-  function literalToVariableInstance(literal: Literal): Variable {
-    return rawToVariableInstance(Math.abs(literal.value))
-  }
-
-  function rawToVariableInstance(n: number): Variable {
-    if (n < 0) throw "ERROR: raw numbers should be >= 0";
-    const variable = new Variable(n)
+  function rawVariableToVariable(rvariable: number): Variable {
+    if (rvariable < 0) throw "ERROR: raw numbers should be >= 0";
+    const variable = new Variable(rvariable)
     return variable;
   }
 
-  function getPolarity(literal: Literal): boolean {
-    return literal.value < 0;
+  function newLiteral(literal: number): Literal {
+    const variableId = Math.abs(literal)
+    if (!variablesMap.has(variableId)) 
+      throw `ERROR: variable - ${variableId} - not found`;
+
+    const variable = variablesMap.get(variableId) as Variable;
+    const polarity = literal < 0 ? 'Negative' : 'Positive';
+    return new Literal(variable, polarity);
   }
 
-  function fromLiteralToVariableRef(literal: Literal): Variable {
-    const v = Math.abs(literal.value)
-    return variablesRef[v];
+  function onLiteralClicked(literal: Literal) {
+    console.log(literal)
+    toggleVariableState(literal.getVariable());
+    console.log(literal.evaluate());
   }
+
+  function toggleVariableState(variable: Variable) {
+    if (variable.isAssigned())
+      variable.negate()
+    else
+      variable.assign(true);
+  }
+
 
 
 </script>
 
+<div>
+  {#each variablesMap as varRef}
+    <Toggle></Toggle>
+  {/each}
+</div>
+
 
 <div class="flex flex-row">
   {#each clause as literal}
-    <LiteralComponent variable={fromLiteralToVariableRef(literal)} polarity={getPolarity(literal)} />
+    <LiteralComponent literal={literal}  />
   {/each}
 </div>
