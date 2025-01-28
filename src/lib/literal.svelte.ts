88867387f383446d4062e12d3c60a852228c4ed0
wilberquito <wbq.software@gmail.com>
2025-01-28 15:29:26 +0100

    feat: definition of literal and variable

---
+++ src/lib/literal.svelte.ts
@@ -0,0 +1,29 @@
+import Variable from '$lib/variable.svelte.ts';
+import { v4 as uuidv4 } from 'uuid';
+
+export type Polarity = 'Positive' | 'Negative';
+
+export default class Literal {
+  private id: string;
+  private variable:  Variable;
+  private polarity: Polarity;
+
+  constructor(variable: Variable, polarity: Polarity) {
+    this.id = uuidv4();
+    this.variable = variable;
+    this.polarity = polarity;
+
+    $inspect(this.variable.isAssigned());
+  }
+
+  public getId() { return this.id; }
+  public getVariable() { return this.variable; }
+  public getPolarity() { return this.polarity; }
+
+  public evaluate(): boolean {
+    let evaluation = this.variable.evaluate();
+    if (this.polarity === 'Negative')
+      evaluation = !evaluation;
+    return evaluation;
+  }
+}
---
+++ src/lib/variable.svelte.ts
@@ -0,0 +1,35 @@
+export default class Variable {
+  private id: number;
+  private evaluation = $state(false);
+  private assigned = $state(false);
+
+  constructor(id: number) {
+    if (id < 0) throw "ERROR: variable ID should be >= 0";
+    this.id = id;
+  }
+
+  public getId(): number { return this.id; }
+  public isAssigned(): boolean { return this.assigned; }
+
+  public evaluate(): boolean {
+    if (!this.isAssigned())
+      throw "ERROR: variable not assigned yet";
+    return this.evaluation;
+  }
+
+  public assign(evaluation: boolean) {
+    this.evaluation = evaluation;
+    this.assigned = true;
+  }
+
+  public negate() {
+    this.evaluation != this.evaluation;
+  }
+}
+
+
+export class IdVariableMap extends Map<number, Variable> {
+  constructor() { 
+    super();
+  }
+}
