83da74236f9b168b99a2b634f367e98f6a912113
wilberquito <wbq.software@gmail.com>
2025-01-29 17:38:10 +0100

    feat: visualizer components

---
+++ src/lib/visualizer/ClauseVisualizerComponent.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type Literal from "$lib/literal.svelte.ts";
+	import LiteralComponent from "$lib/LiteralComponent.svelte";
+
+    interface Props { clause: Literal[] }
+    let { clause }: Props = $props();
+</script>
+
+<div class="flex flex-row">
+  {#each clause as literal (literal.id)}
+    <LiteralComponent literal={literal}  />
+  {/each}
+</div>
