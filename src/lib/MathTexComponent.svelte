e6063316fffe2db37c94cdac137e4f457d2eb173
wilberquito <wbq.software@gmail.com>
2025-01-29 17:02:40 +0100

    feat: defines MathTex component

---
+++ src/lib/MathTexComponent.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { display } from "mathlifier";
+
+    interface Props { equation: string }
+
+    let {equation}: Props = $props()
+</script>
+
+<span>
+    {@html display(equation)}
+</span>
+
+
+<style>
+    span {
+        display: table;
+        margin: 0 auto;
+    }
+</style>
