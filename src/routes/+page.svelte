45e9cd1c64c6e27b05c0789468f4aa7ec4cdc83d
wilberquito <wbq.software@gmail.com>
2025-02-09 15:00:33 +0100

    feat: set default size of icons through svelte context

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
-	import AppLayout from "$lib/AppLayout.svelte";
+	import { setContext } from 'svelte';
+	import AppLayout from '$lib/AppLayout.svelte';
+
+	const iconCtx = {
+		size: 'xs'
+	};
+	setContext('iconCtx', iconCtx);
 
 </script>
+
 
 <AppLayout />
