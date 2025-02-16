34c3c881c811a27cee7c7b80cfb6fe3975b57dfb
wilberquito <wbq.software@gmail.com>
2025-02-16 14:03:11 +0100

    feat: update codes with main

--- src/lib/LogicResolutionComponent.svelte
+++ src/lib/LogicResolutionComponent.svelte
@@ -2,12 +2,13 @@
 	import { Interpretation } from '$lib/interpretation.svelte.ts';
 	import Literal from '$lib/literal.svelte.ts';
 	import Variable, { IdVariableMap } from '$lib/variable.svelte.ts';
+	import CNF, { Clause } from '$lib/cnf.svelte.ts';
 	import ClauseVisualizerComponent from '$lib/visualizer/ClauseVisualizerComponent.svelte';
+	import CnfVisualizerComponent from '$lib/visualizer/CnfVisualizerComponent.svelte';
 	import InterpretationVisualizerComponent from '$lib/visualizer/InterpretationVisualizerComponent.svelte';
 	import { Toggle } from 'flowbite-svelte';
 
 	type RaWCNF = number[][];
-	type CNF = Literal[][];
 
 	const rawCNF: RaWCNF = [
 		[1, -2, 3, 4],
@@ -27,33 +28,32 @@
 	const I = [
 		{
 			id: 1,
-			assigment: true
+			assignment: true
 		},
 		{
 			id: 2,
-			assigment: true
+			assignment: true
 		},
 		{
 			id: 3,
-			assigment: false
+			assignment: false
 		},
 		{
 			id: 4,
-			assigment: false
+			assignment: false
 		}
 	];
 
 	const II = new Interpretation(rawVariables.size);
-	I.forEach(({ id, assigment }) => II.set(variablesMap.get(id) as Variable, assigment));
+	I.forEach(({ id, assignment }) => II.set(variablesMap.get(id) as Variable, assignment));
 
-	const cnf: CNF = rawCNF.map((clause) => clause.map(newLiteral));
-	const clause = cnf[1];
+	const cnf: CNF = new CNF(rawCNF.map((literals) => newClause(literals)));
 
 	assign(II);
 
 	function assign(II: Interpretation) {
-		II.forEach((assigment, variable) => {
-			variable.assign(assigment);
+		II.forEach((assignment, variable) => {
+			variable.assign(assignment);
 		});
 	}
 
@@ -61,6 +61,10 @@
 		if (rvariable < 0) throw 'ERROR: raw numbers should be >= 0';
 		const variable = new Variable(rvariable);
 		return variable;
+	}
+
+	function newClause(literals: number[]): Clause {
+		return new Clause(literals.map(newLiteral));
 	}
 
 	function newLiteral(literal: number): Literal {
@@ -71,49 +75,43 @@
 		return new Literal(variable, polarity);
 	}
 
-	function logicResolution(c1: Literal[], c2: Literal[]): Literal[] {
+	function logicResolution(c1: Clause, c2: Clause): Clause {
 		const resolvedLiterals: Map<number, Literal> = new Map();
+
+		//We need to do this as it follows as the ids of each literal are unique
+		c1.forEach((lit: Literal) => resolvedLiterals.set(lit.toInt(), lit.copy()));
+
 		let foundComplementary = false;
-
-		c1.forEach((l1) => {
-			//We need to do this as it follows as the ids of each literal are unique
-			resolvedLiterals.set(
-				l1.variable.id * (l1.polarity === 'Negative' ? -1 : 1),
-				new Literal(l1.variable, l1.polarity)
-			);
-		});
-
-		c2.forEach((l2) => {
-			const key = l2.variable.id * (l2.polarity === 'Negative' ? -1 : 1);
-			if (resolvedLiterals.has(-key) && !foundComplementary) {
-				//Found complementary, we delete it
-				resolvedLiterals.delete(-key);
+		for (const lit of c2) {
+			const key = lit.toInt();
+			// drops only the first complementary
+			if (resolvedLiterals.has(key * -1) && !foundComplementary) {
+				resolvedLiterals.delete(key * -1);
 				foundComplementary = true;
-			} else if (!resolvedLiterals.has(key)) {
+			} // only adds if it does not exist the literal expressed as natural in the collection
+			else if (!resolvedLiterals.has(key)) {
 				//In case the literals is not inside the resolved clause, we add it
-				resolvedLiterals.set(key, new Literal(l2.variable, l2.polarity));
+				resolvedLiterals.set(key, lit.copy());
 			}
-		});
-		return Array.from(resolvedLiterals.values());
+		}
+		return new Clause(Array.from(resolvedLiterals.values()));
 	}
 </script>
 
-<div>
+<div class="flex-column mt-3 flex justify-center">
 	{#each variables as variable (variable.id)}
 		<span>{variable.id} - {variable.evaluate()}</span>
-		<Toggle bind:checked={variable.evaluation}></Toggle>
+		<Toggle bind:checked={variable.evaluation} class="ml-1 mr-2"></Toggle>
 	{/each}
 </div>
 
 <InterpretationVisualizerComponent {variables} />
-
-{#each cnf as clause}
-	<ClauseVisualizerComponent {clause} />
-{/each}
+<CnfVisualizerComponent {cnf} />
 
 <p>
 	Let's visualize the new clause created by applying logic resolution to the first and second clause
 	of the cnf
 </p>
+<ClauseVisualizerComponent clause={logicResolution(cnf.getClause(0), cnf.getClause(1))} />
 
-<ClauseVisualizerComponent clause={logicResolution(cnf[0], cnf[1])} />
+<p>The cnf is <strong>{cnf.eval()}</strong></p>
