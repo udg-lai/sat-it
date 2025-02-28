504c672281fc007b5a89fa6f35498e24cb61b1b2
Pafefo <pauferrerfont2000@gmail.com>
2025-02-28 18:24:11 +0100

    Variable Collection Code

--- src/lib/transversal/entities/VariableCollection.svelte.ts
+++ src/lib/transversal/entities/VariableCollection.svelte.ts
@@ -1,37 +1,42 @@
-import { makeJust, makeNothing, type Maybe } from "../utils/types/maybe.ts";
-import Variable from "./Variable.svelte.ts";
+import { makeJust, makeNothing, type Maybe } from '../utils/types/maybe.ts';
+import Variable from './Variable.svelte.ts';
 
 export default class VariableCollection {
-  private collection: Variable[];
-  private currentVariable: number = 0;
+	private collection: Variable[];
+	private currentVariable: number = 0;
 
-  constructor(nVariables: number) {
-    this.collection = new Array(nVariables).fill(null).map((_, index) => new Variable(index));
-  }
+	constructor(nVariables: number) {
+		this.collection = new Array(nVariables).fill(null).map((_, index) => new Variable(index));
+	}
 
-  public getCurrentVariable(): Maybe<Variable> {
-    while (this.currentVariable < this.collection.length) {
-      if (!this.collection[this.currentVariable].isAssigned()) {
-        return makeJust(this.collection[this.currentVariable++]);
-      }
-      else {
-        this.currentVariable++;
-      }
-    }
-    return makeNothing();
-  }
+	public getCurrentVariableID(): Maybe<number> {
+		while (this.currentVariable < this.collection.length) {
+			if (!this.collection[this.currentVariable].isAssigned()) {
+				return makeJust(this.collection[this.currentVariable++].getId());
+			} else {
+				this.currentVariable++;
+			}
+		}
+		return makeNothing();
+	}
 
-  public getVariable(index: number): Variable {
-    if (index < 1 || this.collection.length < index)
-      throw "[ERROR]: Trying to obtain an out-of-range variable from the table"
-    else {
-      return this.collection[index - 1];
-    }
-  }
+	public assignVariable(index: number, evaluation: boolean): void {
+		if (index < 1 || this.collection.length < index)
+			throw '[ERROR]: Trying to obtain an out-of-range variable from the table';
+		else {
+			this.collection[index - 1].assign(evaluation);
+		}
+	}
 
-  public restartCounter(): void { this.currentVariable = 0; }
+	public unassignVariable(index: number) {
+		if (index < 1 || this.collection.length < index)
+			throw '[ERROR]: Trying to obtain an out-of-range variable from the table';
+		else {
+			this.collection[index - 1].unassign();
+		}
+	}
 
-
-
-    
+	public restartCounter(): void {
+		this.currentVariable = 0;
+	}
 }
