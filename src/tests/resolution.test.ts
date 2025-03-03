7cfb569a827922dc71b8cdab363977724513721f
wilberquito <wbq.software@gmail.com>
2025-03-03 15:59:40 +0100

    test: adds unit testing for clause resolution

--- src/tests/resolution.test.ts
+++ src/tests/resolution.test.ts
@@ -13,23 +13,69 @@ describe('resolution algorithm', () => {
 		5: new Variable(6)
 	};
 	it('example01', () => {
-		// clause (1):
-		//  [1, -2, -3]
-		// clause (2):
-		//  [1, -2, -3, 4]
-		// resolution:
-		//  [1, -2, -3, 4]
 		const a = new Literal(variables[0], 'Positive');
 		const b = new Literal(variables[1], 'Negative');
 		const c = new Literal(variables[2], 'Negative');
 		const c1 = new Clause([a, b, c]);
+		// clause (c1):
+		//  [1, -2, -3]
 
 		const d = new Literal(variables[0], 'Positive');
 		const e = new Literal(variables[1], 'Negative');
 		const f = new Literal(variables[2], 'Negative');
 		const g = new Literal(variables[3], 'Positive');
 		const c2 = new Clause([d, e, f, g]);
+		// clause (2):
+		//  [1, -2, -3, 4]
 
-		const c3: Clause = c1.resolution(c2);
+		const cr: Clause = c1.resolution(c2);
+		// resolution:
+		//  [1, -2, -3, 4]
+		const cc: Clause = new Clause([a, b, c, g]);
+		expect(cr.equals(cc)).toBe(true);
+	});
+	it('example02', () => {
+		const a = new Literal(variables[0], 'Positive');
+		const b = new Literal(variables[1], 'Negative');
+		const c = new Literal(variables[2], 'Negative');
+		const c1 = new Clause([a, b, c]);
+		// clause (c1):
+		//  [1, -2, -3]
+
+		const d = new Literal(variables[0], 'Positive');
+		const e = new Literal(variables[1], 'Negative');
+		const f = new Literal(variables[2], 'Positive');
+		const g = new Literal(variables[3], 'Positive');
+		const c2 = new Clause([d, e, f, g]);
+		// clause (2):
+		//  [1, -2, 3, 4]
+
+		const cr: Clause = c1.resolution(c2);
+		// resolution:
+		//  [1, -2, 4]
+		const cc: Clause = new Clause([a, b, g]);
+		expect(cr.equals(cc)).toBe(true);
+	});
+	it('example03', () => {
+		const a = new Literal(variables[0], 'Positive');
+		const b = new Literal(variables[1], 'Negative');
+		const c = new Literal(variables[2], 'Negative');
+		const c1 = new Clause([a, b, c]);
+		// clause (c1):
+		//  [1, -2, -3]
+
+		const d = new Literal(variables[0], 'Positive');
+		const e = new Literal(variables[1], 'Negative');
+		const f = new Literal(variables[2], 'Positive');
+		const g = new Literal(variables[3], 'Positive');
+		const c2 = new Clause([d, e, f, g]);
+		// clause (2):
+		//  [1, -2, 3, 4]
+
+		const cr: Clause = c1.resolution(c2);
+		// resolution:
+		//  [1, -2, 4]
+		const cc: Clause = new Clause([b, g, a]);
+		expect(cr.equals(cc)).toBe(true);
 	});
 });
