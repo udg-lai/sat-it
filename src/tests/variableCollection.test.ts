8e83e0b235df8d1d5cde3f91ed2818818e289ef7
Pafefo <pauferrerfont2000@gmail.com>
2025-03-04 09:38:10 +0100

    Refactor

--- src/tests/variableCollection.test.ts
+++ src/tests/variableCollection.test.ts
@@ -2,47 +2,47 @@ import VariablePool from '$lib/transversal/entities/VariablePool.ts';
 import { isJust, unwrapMaybe } from '$lib/transversal/utils/types/maybe.ts';
 import { describe, it, expect } from 'vitest';
 
-const collection: VariablePool = new VariablePool(10);
+const variablePool: VariablePool = new VariablePool(10);
 
 //First we get the variable that we want to give an assignment.
-let index = collection.nextVariableToAssign();
+let index = variablePool.nextVariableToAssign();
 if (isJust(index)) {
 	//We give the assignment
-	collection.assignVariable(unwrapMaybe(index), true);
+	variablePool.assignVariable(unwrapMaybe(index), true);
 }
 
 //Then we call the following index
-index = collection.nextVariableToAssign();
+index = variablePool.nextVariableToAssign();
 if (isJust(index)) {
 	//We give the assignment
-	collection.assignVariable(unwrapMaybe(index), true);
+	variablePool.assignVariable(unwrapMaybe(index), true);
 }
 
-collection.assignVariable(3, false);
-collection.assignVariable(9, false);
-collection.assignVariable(10, true);
+variablePool.assignVariable(3, false);
+variablePool.assignVariable(9, false);
+variablePool.assignVariable(10, true);
 
 //Now we get the following variable to be decided.
-index = collection.nextVariableToAssign();
+index = variablePool.nextVariableToAssign();
 
 if (isJust(index)) {
 	//We give the assignment
-	collection.assignVariable(unwrapMaybe(index), true);
+	variablePool.assignVariable(unwrapMaybe(index), true);
 }
 
 //Restart the counter
-collection.unassignVariable(1);
-index = collection.nextVariableToAssign();
+variablePool.unassignVariable(1);
+index = variablePool.nextVariableToAssign();
 if (isJust(index)) {
-	collection.assignVariable(unwrapMaybe(index), false);
+	variablePool.assignVariable(unwrapMaybe(index), false);
 }
 describe('variableCollection', () => {
 	it('test01', () => {
-		expect(collection.getVariableState(1)).toBe(false);
-		expect(collection.getVariableState(2)).toBe(true);
-		expect(collection.getVariableState(3)).toBe(false);
-		expect(collection.getVariableState(4)).toBe(true);
-		expect(collection.getVariableState(9)).toBe(false);
-		expect(collection.getVariableState(10)).toBe(true);
+		expect(variablePool.getVariableState(1)).toBe(false);
+		expect(variablePool.getVariableState(2)).toBe(true);
+		expect(variablePool.getVariableState(3)).toBe(false);
+		expect(variablePool.getVariableState(4)).toBe(true);
+		expect(variablePool.getVariableState(9)).toBe(false);
+		expect(variablePool.getVariableState(10)).toBe(true);
 	});
 });
