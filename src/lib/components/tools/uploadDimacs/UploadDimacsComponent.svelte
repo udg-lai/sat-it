fad411b0d8bd8318587e28b65cffd30f53c5efea
wilberquito <wbq.software@gmail.com>
2025-03-06 23:17:32 +0100

    wip: view definition of problems

--- src/lib/components/tools/uploadDimacs/UploadDimacsComponent.svelte
+++ src/lib/components/tools/uploadDimacs/UploadDimacsComponent.svelte
@@ -1,50 +1,51 @@
 <script lang="ts">
 	import { AccordionItem, Accordion } from 'flowbite-svelte';
-	import { EyeOutline, UploadOutline } from 'flowbite-svelte-icons';
+	import { EyeOutline, TrashBinOutline, UploadOutline } from 'flowbite-svelte-icons';
+	import { Toggle } from 'flowbite-svelte';
 	import './styles.css';
 
-	function openFileInput(): void {
-		console.log('Uploading file');
+	let problems: string[] = ['problem 01', 'problem 02', 'problem 03'];
+
+	let fileInputRef: HTMLInputElement;
+	function openFileDialog() {
+		fileInputRef.click();
 	}
-	function handleFileChange(): void {}
 </script>
 
 <Accordion flush>
 	<AccordionItem open={true}>
-		<span slot="header">List of problems</span>
-		<ul>
-			<li>
-				<p class="mb-2 text-gray-500 dark:text-gray-400">problema 1</p>
-				<button class="icon">
-					<EyeOutline />
-				</button>
-			</li>
-			<li>
-				<p class="mb-2 text-gray-500 dark:text-gray-400">problema 2</p>
-				<button class="icon">
-					<EyeOutline />
-				</button>
-			</li>
-			<li>
-				<p class="mb-2 text-gray-500 dark:text-gray-400">problema 3</p>
-				<button class="icon">
-					<EyeOutline />
-				</button>
-			</li>
-		</ul>
-	</AccordionItem>
-	<AccordionItem>
 		<span slot="header">Upload problem</span>
 		<div class="container">
 			<div class="file-input-box">
 				<div class="wrapper-file-input flex flex-col">
-					<button class="input-box" onclick={openFileInput}>
+					<button class="input-box" onclick={openFileDialog}>
 						<UploadOutline size="xl" />
-						<input type="file" hidden onchange={handleFileChange} multiple />
+						<input type="file" hidden multiple bind:this={fileInputRef} />
 					</button>
-					<small>Files Supported: DIMACs</small>
+					<small>Files Supported: DIMACS</small>
 				</div>
 			</div>
 		</div>
+	</AccordionItem>
+	<AccordionItem open={false}>
+		<span slot="header">List of problems</span>
+		<ul>
+			{#each problems as problem}
+				<li>
+					<div class="flex">
+						<button class="icon">
+							<TrashBinOutline />
+						</button>
+						<p class="mb-2 text-gray-500 dark:text-gray-400">{problem}</p>
+					</div>
+					<div class="flex">
+						<button class="icon">
+							<EyeOutline />
+						</button>
+						<Toggle class="toggle" />
+					</div>
+				</li>
+			{/each}
+		</ul>
 	</AccordionItem>
 </Accordion>
