94c9ea13da12daf006f8141405435197998216d2
wilberquito <wbq.software@gmail.com>
2025-03-25 19:49:57 +0100

    wip: dimacs preview

---
+++ src/lib/components/tools/dimacs/DimacsComponent.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import InstanceListComponent from '../instance-list/InstanceListComponent.svelte';
+	import DimacsUploaderComponent from '../uploader/DimacsUploaderComponent.svelte';
+	import DimacsViewerComponent from '../viewer/DimacsViewerComponent.svelte';
+</script>
+
+<DimacsUploaderComponent />
+<InstanceListComponent />
+<DimacsViewerComponent />
