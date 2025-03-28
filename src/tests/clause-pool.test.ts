eb5dea7f318c52b941d8820eebabbcfe7ac2dae0
wilberquito <wbq.software@gmail.com>
2025-03-28 12:37:16 +0100

    refactor: just using methods and naming

--- src/tests/clause-pool.test.ts
+++ src/tests/clause-pool.test.ts
@@ -7,44 +7,45 @@ import type { Claims } from '$lib/transversal/utils/parsers/dimacs.ts';
 import { fromClaimsToClause } from '$lib/transversal/utils/utils.ts';
 import { describe, expect, it } from 'vitest';
 
-const dummyExample: Claims = [
+const claims: Claims = [
 	[1, 2, -3, 0],
 	[-2, 3, 0]
 ];
 
-const extraClause: number[] = [-1, -2, 0];
+const anotherClaim: number[] = [-1, -2, 0];
 
 describe('clause pool', () => {
 	it('unresolved', () => {
+		Clause.resetUniqueIdGenerator();
 		const variablePool: VariablePool = new VariablePool(3);
-		const clausePool: ClausePool = new ClausePool(fromClaimsToClause(dummyExample, variablePool));
+		const clausePool: ClausePool = new ClausePool(fromClaimsToClause(claims, variablePool));
 		variablePool.persist(1, true);
 		expect(clausePool.eval()).toBe(Eval.UNRESOLVED);
 	});
 	it('unsat', () => {
-		Clause.setIdCounter(0);
+		Clause.resetUniqueIdGenerator();
 		const variablePool: VariablePool = new VariablePool(3);
-		const clausePool: ClausePool = new ClausePool(fromClaimsToClause(dummyExample, variablePool));
+		const clausePool: ClausePool = new ClausePool(fromClaimsToClause(claims, variablePool));
 		variablePool.persist(1, true);
 		variablePool.persist(2, true);
 		variablePool.persist(3, false);
 		expect(clausePool.eval()).toBe(Eval.UNSAT);
 	});
 	it('sat', () => {
-		Clause.setIdCounter(0);
+		Clause.resetUniqueIdGenerator();
 		const variablePool: VariablePool = new VariablePool(3);
-		const clausePool: ClausePool = new ClausePool(fromClaimsToClause(dummyExample, variablePool));
+		const clausePool: ClausePool = new ClausePool(fromClaimsToClause(claims, variablePool));
 		variablePool.persist(1, true);
 		variablePool.persist(2, false);
 		variablePool.persist(3, true);
 		expect(clausePool.eval()).toBe(Eval.SAT);
 	});
 	it('addition-sat', () => {
-		Clause.setIdCounter(0);
+		Clause.resetUniqueIdGenerator();
 		const variablePool: VariablePool = new VariablePool(3);
-		const clausePool: ClausePool = new ClausePool(fromClaimsToClause(dummyExample, variablePool));
+		const clausePool: ClausePool = new ClausePool(fromClaimsToClause(claims, variablePool));
 		const literalCollection: Literal[] = [];
-		extraClause.forEach((value) => {
+		anotherClaim.forEach((value) => {
 			if (value !== 0) {
 				literalCollection.push(
 					new Literal(variablePool.get(Math.abs(value)), value < 0 ? 'Negative' : 'Positive')
@@ -58,22 +59,20 @@ describe('clause pool', () => {
 		expect(clausePool.eval()).toBe(Eval.SAT);
 	});
 	it('addition-sat', () => {
-		Clause.setIdCounter(0);
+		Clause.resetUniqueIdGenerator();
 		const variablePool: VariablePool = new VariablePool(3);
-		const clausePool: ClausePool = new ClausePool(fromClaimsToClause(dummyExample, variablePool));
-		expect(clausePool.poolCapacity()).toBe(2);
-		const literalCollection: Literal[] = [];
-		extraClause.forEach((value) => {
+		const clausePool: ClausePool = new ClausePool(fromClaimsToClause(claims, variablePool));
+		expect(clausePool.size()).toBe(2);
+		const literals: Literal[] = [];
+		anotherClaim.forEach((value) => {
 			if (value !== 0) {
-				literalCollection.push(
+				literals.push(
 					new Literal(variablePool.get(Math.abs(value)), value < 0 ? 'Negative' : 'Positive')
 				);
 			}
 		});
-		clausePool.addClause(new Clause(literalCollection));
-		expect(clausePool.get(2).id).toBe(2);
-		clausePool.removeClause(1);
-		expect(clausePool.get(1).id).toBe(1);
-		expect(Clause.idCounter).toBe(2);
+		clausePool.addClause(new Clause(literals));
+		expect(clausePool.size()).toBe(3);
+		expect(Clause.nextUniqueId()).toBe(3);
 	});
 });
