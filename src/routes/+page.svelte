658bc59e7ac25f654f608c0389dd100bd3aab159
Pafefo <pauferrerfont2000@gmail.com>
2025-04-04 19:27:24 +0200

    Now the problem changes with the toggles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,50 +4,21 @@
 	import ToolsComponent from '$lib/components/tools/ToolsComponent.svelte';
 	import ScrollableComponent from '$lib/components/ScrollableComponent.svelte';
 	import ToastComponent from '$lib/components/ToastComponent.svelte';
-	import { disableContextMenu, fromClaimsToClause } from '$lib/transversal/utils/utils.ts';
+	import { disableContextMenu } from '$lib/transversal/utils/utils.ts';
 	import { toasts } from '$lib/store/toasts.store.ts';
-	import {
-		instanceStore,
-		initializeInstancesStore,
-		type InteractiveInstance
-	} from '$lib/store/instances.store.ts';
+	import { initializeInstancesStore, setInstanceToSolve } from '$lib/store/instances.store.ts';
 	import { logError } from '$lib/transversal/utils/logging.ts';
-	import VariablePool from '$lib/transversal/entities/VariablePool.ts';
-	import { updateProblem } from '$lib/store/problem.store.ts';
 	import { onMount } from 'svelte';
 	import AppComponent from '$lib/components/AppComponent.svelte';
-	import ClausePool from '$lib/transversal/entities/ClausePool.svelte.ts';
 
 	onMount(() => {
 		initializeInstancesStore()
-			.then(setDefaultInstanceToSolve)
+			.then(() => setInstanceToSolve(0))
 			.catch(() =>
 				logError(`Preloaded instances`, `Could not fetch preloaded instances correctly`)
 			);
 	});
 
-	function setDefaultInstanceToSolve() {
-		// pre: only executed if there are problems to set as default
-		const instances: InteractiveInstance[] = $instanceStore;
-		if (instances.length > 0) {
-			const { summary } = instances[0];
-			const { claims } = summary;
-
-			const variables: VariablePool = new VariablePool(summary.varCount);
-			const clauses: ClausePool = new ClausePool(fromClaimsToClause(claims.simplified, variables));
-
-			const pools = {
-				variables,
-				clauses
-			};
-
-			const algorithm = () => console.log('new algorithm');
-
-			const problem = { pools, algorithm };
-
-			updateProblem(problem);
-		}
-	}
 </script>
 
 <svelte:body oncontextmenu={disableContextMenu} />
