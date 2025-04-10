94297ac2112c8b93f4a1275be7fd26f8bc02a708
Pafefo <pauferrerfont2000@gmail.com>
2025-04-10 18:02:55 +0200

    Prettier

--- src/lib/components/tools/debugger/GeneralDebuggerButtonsComponent.svelte
+++ src/lib/components/tools/debugger/GeneralDebuggerButtonsComponent.svelte
@@ -4,6 +4,6 @@
 
 <div class="flex flex-row place-content-around">
 	<button class="general-btn"> Solve </button>
-	<button class="general-btn mr-1 ml-1"> Solve Trail </button>
-  <button class="general-btn"> Undo </button>
+	<button class="general-btn ml-1 mr-1"> Solve Trail </button>
+	<button class="general-btn"> Undo </button>
 </div>
