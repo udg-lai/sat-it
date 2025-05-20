bd5fcb4496bb1690c7eaf6f6f89fb995bd9ebab6
Pafefo <pauferrerfont2000@gmail.com>
2025-05-20 23:36:20 +0200

    Prettier

--- src/lib/store/statistics.svelte.ts
+++ src/lib/store/statistics.svelte.ts
@@ -1,52 +1,58 @@
 export interface Statistics {
-  noDecisions: number;
-  noBacktrackings: number;
-  noUnitPropagations: number;
+	noDecisions: number;
+	noBacktrackings: number;
+	noUnitPropagations: number;
 }
 
 const statistics: Statistics = $state({
-  noDecisions: 0,
-  noBacktrackings: 0,
-  noUnitPropagations: 0
+	noDecisions: 0,
+	noBacktrackings: 0,
+	noUnitPropagations: 0
 });
 
 export const increaseNoDecisions = (): void => {
-  statistics.noDecisions += 1;
+	statistics.noDecisions += 1;
 };
 
 export const increaseNoBacktrackings = (): void => {
-  statistics.noBacktrackings += 1;
+	statistics.noBacktrackings += 1;
 };
 
 export const increaseNoUnitPropgations = (): void => {
-  statistics.noUnitPropagations += 1;
+	statistics.noUnitPropagations += 1;
 };
 
 export const updateStatistics = (newStatistics: Statistics): void => {
-  statistics.noDecisions = newStatistics.noDecisions;
-  statistics.noBacktrackings = newStatistics.noBacktrackings;
-  statistics.noUnitPropagations = newStatistics.noUnitPropagations;
-}
+	statistics.noDecisions = newStatistics.noDecisions;
+	statistics.noBacktrackings = newStatistics.noBacktrackings;
+	statistics.noUnitPropagations = newStatistics.noUnitPropagations;
+};
 
 export const updateNoDecisions = (newDecisions: number): void => {
-  statistics.noDecisions = newDecisions;
+	statistics.noDecisions = newDecisions;
 };
 
 export const updateNoBacktracking = (newBacktrackings: number): void => {
-  statistics.noBacktrackings = newBacktrackings;
+	statistics.noBacktrackings = newBacktrackings;
 };
 
 export const updateNoUnitPropagations = (newUnitPropagations: number): void => {
-  statistics.noUnitPropagations = newUnitPropagations;
+	statistics.noUnitPropagations = newUnitPropagations;
 };
 
 export const resetStatistics = (): void => {
-  statistics.noDecisions = 0;
-  statistics.noBacktrackings = 0;
-  statistics.noUnitPropagations = 0;
+	statistics.noDecisions = 0;
+	statistics.noBacktrackings = 0;
+	statistics.noUnitPropagations = 0;
 };
 
-export const getStatistics = () => { return { noDecisions: statistics.noDecisions, noBacktrackings: statistics.noBacktrackings, noUnitPropagations: statistics.noDecisions } as Statistics };
+export const getStatistics = () => {
+	return {
+		noDecisions: statistics.noDecisions,
+		noBacktrackings: statistics.noBacktrackings,
+		noUnitPropagations: statistics.noDecisions
+	} as Statistics;
+};
 export const getNoDecisions = () => statistics.noDecisions;
 export const getNoBacktarcking = () => statistics.noBacktrackings;
 export const getNoUnitPropagations = () => statistics.noUnitPropagations;
