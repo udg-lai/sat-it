3dab2d8036c108ef2a4dab69beb72878995bb9cb
Pafefo <pauferrerfont2000@gmail.com>
2025-05-27 13:00:03 +0200

    wip

--- src/lib/components/settings/EngineComponent.svelte
+++ src/lib/components/settings/EngineComponent.svelte
@@ -6,17 +6,17 @@
 	const headingClass: string =
 		'h-20 w-full flex items-center justify-center gap-2 text-md border-b';
 	const iconClass = { size: 'xl' };
-	const bodyClass: string = 'h-full w-full p-4 flex flex-col gap-3';
+	const bodyClass: string = 'h-full w-full p-4 flex flex-col gap-3 overflow-hidden';
 </script>
 
 <div class="grid h-full w-full grid-cols-3 divide-x-4">
-	<div class="flex flex-col">
+	<div class="flex flex-col overflow-hidden">
 		<AlgorithmComponent {headingClass} {iconClass} {bodyClass} />
 	</div>
-	<div class="flex flex-col">
+	<div class="flex flex-col overflow-hidden">
 		<BreakingpointComponent {headingClass} {iconClass} {bodyClass} />
 	</div>
-	<div class="flex flex-col">
+	<div class="flex flex-col overflow-hidden">
 		<ParametersComponent {headingClass} {iconClass} {bodyClass} />
 	</div>
 </div>
