cc3ed3b3a01c5d3c414adf25b6c1e52111567721
Pafefo <pauferrerfont2000@gmail.com>
2025-05-30 11:46:47 +0200

    test

--- src/lib/components/debugger/DebuggerComponent.svelte
+++ src/lib/components/debugger/DebuggerComponent.svelte
@@ -3,6 +3,9 @@
 	import type { StateFun, StateInput } from '$lib/machine/StateMachine.svelte.ts';
 	import { getProblemStore, type Problem } from '$lib/store/problem.svelte.ts';
 	import { getSolverMachine } from '$lib/store/stateMachine.svelte.ts';
+	import Variable from '$lib/transversal/entities/Variable.svelte.ts';
+	import VariableAssignment from '$lib/transversal/entities/VariableAssignment.ts';
+	import ChildlessDecisionComponent from '../assignment/ChildlessDecisionComponent.svelte';
 	import AutomaticDebugger from './AutomaticDebuggerComponent.svelte';
 	import AutoModeComponent from './AutoModeComponent.svelte';
 	import ConflictDetectionDebugger from './ConflictDetectionDebuggerComponent.svelte';
@@ -20,6 +23,9 @@
 	let enableConflictDetection = $derived(solverMachine.detectingConflict());
 	let finished = $derived(solverMachine.completed());
 	let inAutoMode = $derived(solverMachine.isInAutoMode());
+
+
+	let test_variable: VariableAssignment = VariableAssignment.newManualAssignment(new Variable(9999, false));
 </script>
 
 <debugger>
@@ -51,6 +57,10 @@
 		<GeneralDebuggerButtons {finished} backtrackingState={enableBacktracking} />
 	{/if}
 </debugger>
+<div class="ml-2">
+	<ChildlessDecisionComponent assignment={test_variable}/>
+</div>
+
 
 <style>
 	debugger {
