35011a7727d08d2c3aea990a7d81a51224005671
Pau <pauferrerfont2000@gmail.com>
2025-06-23 13:09:16 +0200

    Fix: Now the speed bar is displayed in all browsers

--- src/lib/components/debugger/AutoModeComponent.svelte
+++ src/lib/components/debugger/AutoModeComponent.svelte
@@ -21,6 +21,10 @@
 	let step = STEP_DELAY;
 	let delay = $state(getBaselineDelay());
 
+	// Chromium does not control the part of the range that is "filled" so we need to create a gradient and add it to the background
+	let percentage: number = $derived(((delay - min) / (max - min)) * 100);
+	let chromiumBackground: string = $derived(`linear-gradient(to right, var(--conflict-color) 0%, var(--conflict-color) ${percentage}%, var(--inspecting-color) ${percentage}%, var(--inspecting-color) 100%)`);
+
 	let mappedDelay = $derived(
 		MIN_DELAY_MS * (MAX_DELAY_MS / MIN_DELAY_MS) ** ((max - delay) / (max - min))
 	);
@@ -35,7 +39,7 @@
 		<DynamicRender component={StopOutline} props={{ size: 'md' }} />
 	</button>
 	<div class="range">
-		<Range id="range-steps" {min} {max} bind:value={delay} {step} size="sm" />
+		<Range id="range-steps" {min} {max} bind:value={delay} {step} size="sm" style="background: {chromiumBackground};"/>
 	</div>
 </auto-mode>
 
@@ -51,13 +55,29 @@
 
 	.range {
 		flex: 1;
+		display: flex;
+		align-items: center;
 	}
 
-	:global(.range input) {
-		background-color: linear-gradient(to right, #a8a7a7, var(--inspecting-color));
+	:global(.range input[type="range"]) {
 		-webkit-appearance: none;
 		appearance: none;
-		height: 3px;
-		border-radius: 2px;
+		/* modify the background color in chromium and macOS web browser */
+		&::-webkit-slider-runnable-track {
+			height: 3px;
+		}
+		/* modify the highlighted part of the track in firefox web browser */
+		&::-moz-range-progress  {
+			background-color: var(--conflict-color);
+		}
+		/* modify the thumb in chromium and macOS web browser */
+		&::-webkit-slider-thumb {
+			background-color: var(--conflict-color);
+			margin-top: -5px;
+		}
+		/* modify the thumb in firefox web browser */
+		&::-moz-range-thumb {
+			background-color: var(--conflict-color);
+		}
 	}
 </style>
