65ed616dc55fb78599fd4d3031a6821fd4d3441e
Pau <pauferrerfont2000@gmail.com>
2025-07-03 20:32:25 +0200

    feat: literal no longer need a copy methode as it does not hap an id

--- src/lib/algorithms/resolution.ts
+++ src/lib/algorithms/resolution.ts
@@ -4,22 +4,21 @@ import type Literal from '$lib/entities/Literal.svelte.ts';
 export default function logicResolution(c1: Clause, c2: Clause): Clause {
 	const resolvedLiterals: Map<number, Literal> = new Map();
 
-	//We need to do this as it follows as the ids of each literal are unique
+	//The first clause literals are inserted in the map.
 	for (const lit of c1) {
-		resolvedLiterals.set(lit.toInt(), lit.copy());
+		resolvedLiterals.set(lit.toInt(), lit);
 	}
 
 	let foundComplementary = false;
 	for (const lit of c2) {
 		const litId = lit.toInt();
-		// drops only the first complementary
+		// Only the first complementary is dropped.
 		if (resolvedLiterals.has(litId * -1) && !foundComplementary) {
 			resolvedLiterals.delete(litId * -1);
 			foundComplementary = true;
-		} // only adds if it does not exist the literal expressed as natural in the collection
+		} // In case the literal is not inside the resolved clause, it is added.
 		else if (!resolvedLiterals.has(litId)) {
-			//In case the literals is not inside the resolved clause, we add it
-			resolvedLiterals.set(litId, lit.copy());
+			resolvedLiterals.set(litId, lit);
 		}
 	}
 	return new Clause(Array.from(resolvedLiterals.values()));
