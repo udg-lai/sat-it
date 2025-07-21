7648a3733bbe003555e41a5fbccde10ad2175d2b
Pau <pauferrerfont2000@gmail.com>
2025-07-21 17:43:15 +0200

    wip: aknowlegements

--- src/lib/components/settings/legend/LegendComponent.svelte
+++ src/lib/components/settings/legend/LegendComponent.svelte
@@ -3,20 +3,48 @@
 	import TrailLegend from './TrailLegendComponent.svelte';
 	import ToolsLegend from './ToolsLegendComponent.svelte';
 </script>
+<legend-view>
+	<legend>
+		<div class="content-container">
+			<ButtonsLegend />
+		</div>
+		<div class="division-container"></div>
+		<div class="content-container">
+			<TrailLegend />
+		</div>
+		<div class="division-container"></div>
+		<div class="content-container">
+			<ToolsLegend />
+		</div>
+	</legend>
+	<acknowledge>
+		<span>This project was made by Pau Ferrer i Font and Wilber B. Quito under the supervision of Dr. Mateu Villaret Auselle and Jordi Coll Caballero. </span>
+	</acknowledge>
+</legend-view>
 
-<div class="content-container">
-	<ButtonsLegend />
-</div>
-<div class="division-container"></div>
-<div class="content-container">
-	<TrailLegend />
-</div>
-<div class="division-container"></div>
-<div class="content-container">
-	<ToolsLegend />
-</div>
+
 
 <style>
+	legend-view {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		height: 100%;
+	}
+	legend {
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		height: calc(100% - 50px);
+	}
+	acknowledge {
+		height: 50px;
+		width: 100%;
+		display: flex;
+		border-top: 1px solid var(--border-color);
+		align-items: center;
+		background-color: var(--main-bg-color);
+	}
 	.division-container {
 		height: 100%;
 		width: 2px;
