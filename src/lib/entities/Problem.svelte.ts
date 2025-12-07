90e90cff53637b6378e3d6cfee6e7bbff589b51e
wilberquito <typingwil@gmail.com>
2025-12-07 16:45:41 +0100

    feat: watch table entity

--- src/lib/entities/Problem.svelte.ts
+++ src/lib/entities/Problem.svelte.ts
@@ -1,26 +1,19 @@
 import type { DimacsInstance } from '$lib/instances/dimacs-instance.interface.ts';
-import { logError, logFatal } from '$lib/states/toasts.svelte.ts';
-import type { ClauseTag, Lit } from '$lib/types/types.ts';
+import { logError } from '$lib/states/toasts.svelte.ts';
+import type { ClauseRef, Lit } from '$lib/types/types.ts';
 import type Clause from './Clause.svelte.ts';
 import ClausePool from './ClausePool.svelte.ts';
-import type Literal from './Literal.svelte.ts';
-import OccurrenceTable from './OccurrenceTable.svete.ts';
+import OccurrenceTable from './OccurrenceTable.svelte.ts';
 import type { Trail } from './Trail.svelte.ts';
 import type Variable from './Variable.svelte.ts';
 import { VariablePool } from './VariablePool.svelte.ts';
-
-
-export interface Watch {
-	clauseTag: ClauseTag;
-	literalPos: number;
-}
-export type WatchTable = Map<Lit, Watch[]>;
+import WatchTable from './WatchTable.svelte.ts';
 
 export default class Problem {
 	private variables: VariablePool = $state(new VariablePool(0));
 	private clauses: ClausePool = $state(new ClausePool());
 	private occurrencesTable: OccurrenceTable = $state(new OccurrenceTable());
-	private watchTable: WatchTable = $state(new Map<Lit, Watch[]>());
+	private watchTable: WatchTable = $state(new WatchTable());
 
 	constructor(instance: DimacsInstance | undefined = undefined) {
 		if (instance !== undefined) this.syncWithDimacsInstance(instance);
@@ -43,7 +36,7 @@ export default class Problem {
 		this.variables = new VariablePool(varCount);
 		this.clauses = ClausePool.buildFrom(claims, this.variables);
 		this.occurrencesTable = new OccurrenceTable(this.clauses.getClauses());
-		this.watchTable = this.makeWatchTable();
+		this.watchTable = new WatchTable(this.clauses.getClauses());
 	}
 
 	syncWithTrail(trail: Trail) {
@@ -52,8 +45,7 @@ export default class Problem {
 
 		// Then, we assign the variables as in the trail
 		// this is possible because the assignments in the trail are a copy of the variables in the pool
-		for (const assignment of trail)
-		{
+		for (const assignment of trail) {
 			const variable: Variable = assignment.getVariable();
 			this.variables.assign(variable.toInt(), variable.getAssignment());
 		}
@@ -76,45 +68,5 @@ export default class Problem {
 	addClause(clause: Clause) {
 		this.clauses.addClause(clause);
 		this.occurrencesTable.addOccurrences(clause);
-	}
-
-	private makeWatchTable(): WatchTable {
-		// The clause allocator (i.e., ClausePool) must have already assigned tags to all clauses
-		const watchTable: WatchTable = new Map();
-
-		for (const clause of this.clauses.getClauses()) {
-
-			// If the clause is unit, skip it
-			if (clause.isUnit()) continue;
-
-			// Otherwise, get the first two literals to watch
-			const literals: Literal[] = clause.getLiterals().slice(0, 2);
-
-			const clauseTag = clause.getTag();
-			if (clauseTag === undefined)
-				logFatal('Making watch table', 'Clause without tag found when making watch table');
-
-			// Watch the first two literals in the clause
-			for (let i = 0; i < literals.length; i++) {
-				const literalId: number = literals[i].toInt();
-				const watch: Watch = {
-					clauseTag: clauseTag,
-					literalPos: i
-				};
-				if (watchTable.has(literalId)) {
-					const watches = watchTable.get(literalId);
-					watches?.push(watch);
-				} else {
-					const watches: Watch[] = [watch];
-					watchTable.set(literalId, watches);
-				}
-			}
-		}
-		return watchTable;
-	}
-
-	private updateWatchTable(): void {
-		// This function should be called when the current assignment is modified
-		// or the clause pool is modified because some learned clauses are removed.
 	}
 }
---
+++ src/lib/entities/WatchTable.svelte.ts
@@ -0,0 +1,54 @@
+import { logFatal } from '$lib/states/toasts.svelte.ts';
+import { SvelteMap } from 'svelte/reactivity';
+import type Clause from './Clause.svelte.ts';
+import type { ClauseRef, Lit } from '$lib/types/types.ts';
+import type Literal from './Literal.svelte.ts';
+
+export interface Watch {
+	cRef: ClauseRef;
+	blocker: Literal; // The literal that is currently satisfying the clause
+}
+
+export default class WatchTable {
+	private table: SvelteMap<Lit, Watch[]> = new SvelteMap();
+
+	constructor(clauses: Clause[] = []) {
+		this.table = this.makeWatchTable(clauses);
+	}
+
+	syncWithClauses(clauses: Clause[]): void {
+		this.table = this.makeWatchTable(clauses);
+	}
+
+	private makeWatchTable(clauses: Clause[]): SvelteMap<Lit, Watch[]> {
+		const table: SvelteMap<Lit, Watch[]> = new SvelteMap();
+
+		// The clause allocator (i.e., ClausePool) must have already assigned tags to all clauses
+		for (const clause of clauses) {
+			// The 2-watch scheme makes no sense for unit clauses
+			if (clause.size() === 1) continue;
+
+			// Otherwise, get the first two literals to watch
+			const literals: Literal[] = clause.getLiterals();
+
+			const cRef = clause.getCRef();
+			if (cRef === undefined)
+				logFatal('Making watch table', 'Clause without tag found when making watch table');
+
+			// Watch the first two literals in the clause
+			for (let i = 0; i < 2; i++) {
+				const literal: Literal = literals[i];
+				const watch: Watch = {
+					cRef: cRef,
+					blocker: literal
+				};
+				const litId: number = literal.toInt();
+				if (!this.table.has(litId)) {
+					this.table.set(litId, []);
+				}
+				this.table.get(litId)?.push(watch);
+			}
+		}
+		return table;
+	}
+}
