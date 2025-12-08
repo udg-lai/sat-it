653b9a97e26fee612bca18edafe203aeb52edf08
pafefo <pauferrerfont2000@gmail.com>
2025-12-08 20:27:11 +0100

    change

--- src/lib/components/debugger/ConflictAnalysisDebuggerComponent.svelte
+++ src/lib/components/debugger/ConflictAnalysisDebuggerComponent.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import ConflictAnalysisComponent from './buttons/ConflictAnalysisComponent.svelte';
-	import StepComponent from './buttons/StepComponent.svelte';
+	import ResolutionComponent from './buttons/StepComponent.svelte';
 
 	let resolutionIcon = '/icons/Resolution.svg';
 </script>
 
 <conflict-analysis-debugger>
-	<StepComponent myClass={'conflict-btn'} icon={resolutionIcon} alt="Resolve following clause" />
+	<ResolutionComponent myClass={'conflict-btn'} icon={resolutionIcon} alt="Resolve following clause" />
 
 	<ConflictAnalysisComponent />
 </conflict-analysis-debugger>
